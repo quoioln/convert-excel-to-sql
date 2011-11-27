aef6e031c18df8d97b1f344b5f9c1cf35de7377d
grega.vrbancic <grega.vrbancic@gmail.com>
2011-11-27 20:11:42 +0000

    written class CityPost

---
+++ excelToSQL/src/exceltosql/CityPost.java
@@ -0,0 +1,26 @@
+package exceltosql;
+
+/**
+ *
+ * @author grega vrbancic
+ */
+public class CityPost {
+    private String city;
+    private Integer post;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getPost() {
+        return post;
+    }
+
+    public void setPost(Integer post) {
+        this.post = post;
+    }   
+}
