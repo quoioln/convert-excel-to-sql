bdc039e2df2ccbf5ce56e64f6baf0998f3d43a5c
grega.vrbancic <grega.vrbancic@gmail.com>
2011-11-28 10:14:02 +0000

    added writting to file, repaired known bugs, etc.

--- excelToSQL/src/exceltosql/ExcelToSQL.java
+++ excelToSQL/src/exceltosql/ExcelToSQL.java
@@ -1,8 +1,13 @@
 package exceltosql;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -10,7 +15,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
- * @author grega vrabancic
+ * @author grega vrbancic
  */
 public class ExcelToSQL {
 
@@ -19,20 +24,31 @@ public class ExcelToSQL {
      */
     public static void main(String[] args) throws IOException {
         String filename = "city_post.xlsx";
+        String db;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Database name: ");
+        db=scan.nextLine();
         
+        try {
+            convertToSQL(filename, db);
+        } catch (Exception ex) {
+            Logger.getLogger(ExcelToSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public static void convertToSQL(String filename) throws Exception{
+    public static void convertToSQL(String filename, String db) throws Exception{
         String[] attributes=new String[2];
-        CityPost[] cityPost=readSheet(filename, attributes);
+        if(readSheet(filename, attributes, db)){
+            System.out.println("Convert done!");
+        }
     }
     
-    public static CityPost[] readSheet(String filename, String[] attributes) throws Exception{
+    public static boolean readSheet(String filename, String[] attributes, String db) throws Exception{
         FileInputStream fis = null;
-        CityPost[] cityPosts=null;
         try {
             fis = new FileInputStream(filename);
-            
+            FileWriter fstream = new FileWriter("SQL.sql");
+            BufferedWriter out = new BufferedWriter(fstream);
             XSSFWorkbook workbook = new XSSFWorkbook(fis);
             XSSFSheet sheet = workbook.getSheetAt(0);
             Iterator rows = sheet.rowIterator();
@@ -41,8 +57,8 @@ public class ExcelToSQL {
             System.out.println("Loading...");
             boolean firstRow=true;
             Integer counter=0;
-            Integer counterRows=-1;
-            cityPosts=new CityPost[number];
+            String city;
+            Integer post=0;
             while (rows.hasNext())
             {
                 XSSFRow row = ((XSSFRow) rows.next());
@@ -52,16 +68,22 @@ public class ExcelToSQL {
                     XSSFCell cell = (XSSFCell) cells.next();
                     if(firstRow){
                         attributes[counter]=cell.getStringCellValue();
+                        counter++;
                     }
                     else{
-                        cityPosts[counterRows].setCity(cell.getStringCellValue());
-                        cityPosts[counterRows].setPost(Integer.parseInt(cell.getRawValue()));
+                        city=cell.getStringCellValue();
+                        if(cells.hasNext()){
+                           cell = (XSSFCell) cells.next();
+                           post = (int)cell.getNumericCellValue();   
+                        }
+                        out.write("INSERT INTO " + db + ".city_post (" + attributes[0] + ", "
+                                + attributes[1] + ") VALUES ('" + city + "', " + post +");\n");
                     }
                 }
-                firstRow=false;
-                counter++;
-                counterRows++;
+                firstRow=false;               
              }
+            fstream.close();
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -69,6 +91,6 @@ public class ExcelToSQL {
                 fis.close();
             }
         }
-        return cityPosts;
+        return false;
     }
 }
